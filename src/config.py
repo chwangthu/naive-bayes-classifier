f35364a5f015789f7a406424b28b8e8e17e9c374
chwangthu <chwangthu@gmail.com>
2019-03-24 20:05:12 +0800

    Finish data process part

---
+++ src/config.py
@@ -0,0 +1,7 @@
+#-*- coding:utf-8 -*-
+data_dir = u'../trec06-utf8'
+raw_data_dir = u'../trec06-utf8/data'
+cut_data_dir = u'../trec06-utf8/data_cut'
+fold_data_dir = u'../dataset'
+label_file = u'../trec06c-utf8/label/index'
+random_seed = 1999
---
+++ src/parser.py
@@ -0,0 +1,43 @@
+#-*- coding:utf-8 -*-
+import os
+import config
+import random
+import _pickle as pickle
+
+def get_label():
+    f = open(config.label_file)
+    lines = f.readlines()
+    f.close()
+
+    label_dic = {}
+    for line in lines:
+        line = line[:-1] # remove line escape
+        line = line.split(' ')
+        label = line[0]
+        file = config.cut_data_dir + line[1][7:] 
+        label_dic[file] = label
+        # print(label_dic[label])
+    # print(label_dic)
+    return label_dic
+
+def divide_into_folds(label_dic, num_folds=5):
+    dataset = []
+    random.seed(config.random_seed)
+    for _ in range(num_folds):
+        dataset.append([])
+    for item in label_dic:
+        # print(item)
+        t = random.randint(0, 4)
+        dataset[t].append(item + " " + label_dic[item] + "\n")
+    if not os.path.exists(config.fold_data_dir):
+        os.makedirs(config.fold_data_dir)
+    for i in range(num_folds):
+        f = open(config.fold_data_dir + "/" + str(i), 'w')
+        f.writelines(dataset[i])
+        f.close()
+
+
+if __name__ == "__main__":
+    label_dic = get_label()
+    divide_into_folds(label_dic)
+    pass
